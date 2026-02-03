eaba2e225c9ef405cc3fdde099c9dd191f1d2608
arvindkaur83 <72114508+arvindkaur83@users.noreply.github.com>
2026-02-03 08:46:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,5 +90,6 @@ def api_news():
 
     return jsonify(articles)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available, default to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
